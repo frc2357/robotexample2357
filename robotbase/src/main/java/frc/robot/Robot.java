64ff0cc7ff94a286614b82c81e788156974c2855
Kevin Killingsworth <kevin@redfenix.com>
2019-10-29 01:09:47 -0500

    Add limit switch example

--- robotbase/src/main/java/frc/robot/Robot.java
+++ robotbase/src/main/java/frc/robot/Robot.java
@@ -11,9 +11,9 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+
+import com.systemmeltdown.examples.subsystems.LimitSwitchExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -29,16 +29,19 @@ public class Robot extends TimedRobot {
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
+  LimitSwitchExampleSubsystem m_limitSwitchExampleSubsystem;
+
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
    */
   @Override
   public void robotInit() {
-    m_oi = new OI();
-    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
-    // chooser.addOption("My Auto", new MyAutoCommand());
-    SmartDashboard.putData("Auto mode", m_chooser);
+    // Example Subsystems
+    // Uncomment the desired subsystem below to test and observe.
+
+    // Limit Switch: Connect a limit switch to DIO port 0
+    //m_limitSwitchExampleSubsystem = new LimitSwitchExampleSubsystem(0, false);
   }
 
   /**
