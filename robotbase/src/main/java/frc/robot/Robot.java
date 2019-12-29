a021443a7820fcf129197118c85db68f8c464ded
Kevin Killingsworth <kevin@redfenix.com>
2019-12-29 15:10:32 -0600

    Add CanIdMap, remove example code
    This adds the CanIdMap that was copied/pasted from the robotlib
    It also removes pre-generated example code we don't need.
    And it adds some printlns

--- robotbase/src/main/java/frc/robot/Robot.java
+++ robotbase/src/main/java/frc/robot/Robot.java
@@ -8,10 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import frc.robot.subsystems.ExampleSubsystem;
 
 import com.systemmeltdown.examples.subsystems.LimitSwitchExampleSubsystem;
 
@@ -23,13 +20,7 @@ import com.systemmeltdown.examples.subsystems.LimitSwitchExampleSubsystem;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
-  public static OI m_oi;
-
-  Command m_autonomousCommand;
-  SendableChooser<Command> m_chooser = new SendableChooser<>();
-
-  LimitSwitchExampleSubsystem m_limitSwitchExampleSubsystem;
+  private LimitSwitchExampleSubsystem m_limitSwitchExampleSubsystem;
 
   /**
    * This function is run when the robot is first started up and should be
@@ -37,11 +28,13 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    // Example Subsystems
-    // Uncomment the desired subsystem below to test and observe.
+    System.out.println("-- robotInit --");
 
     // Limit Switch: Connect a limit switch to DIO port 0
     //m_limitSwitchExampleSubsystem = new LimitSwitchExampleSubsystem(0, false);
+
+    // This prevents an initial watchdog overrun.
+    Scheduler.getInstance().run();
   }
 
   /**
@@ -63,6 +56,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void disabledInit() {
+    System.out.println("-- disabledInit --");
   }
 
   @Override
@@ -83,19 +77,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_chooser.getSelected();
-
-    /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector",
-     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-     * = new MyAutoCommand(); break; case "Default Auto": default:
-     * autonomousCommand = new ExampleCommand(); break; }
-     */
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.start();
-    }
+    System.out.println("-- autonomousInit --");
   }
 
   /**
@@ -108,13 +90,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+    System.out.println("-- teleopInit --");
   }
 
   /**
